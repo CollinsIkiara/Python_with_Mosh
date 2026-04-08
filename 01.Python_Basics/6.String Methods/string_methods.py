f7c23003154a7646df0812f7793ac11a46e0f360
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-04-08 17:42:14 +0300

    Initial commit

---
+++ 01.Python_Basics/6.String Methods/string_methods.py
@@ -0,0 +1,11 @@
+# String Methods in Python
+
+course = "Python for Beginners"
+print(len(course))
+print(course)
+print(course.upper())
+print(course.lower())
+print(course.title()) # Converts the first character of each word to uppercase
+print(course.find("Beginners")) # Returns the index of the first occurrence of the specified substring.
+print(course.replace("Beginners", "Absolute Beginners"))
+print("Python" in course) # The 'in' operator checks if a substring exists within a string and returns True or False.
