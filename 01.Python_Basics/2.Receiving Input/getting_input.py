2633fb89b916ec03603940ef89388dd3a8c9af34
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-04-08 16:19:38 +0300

    Initial commit

---
+++ 01.Python_Basics/2.Receiving Input/getting_input.py
@@ -0,0 +1,5 @@
+# The following simple program is to get an input from the user, store it in variables and then print it out.
+
+name = input('What is your name? ')
+colour = input('What is your favourite colour? ')
+print(name + ' likes ' + colour)
