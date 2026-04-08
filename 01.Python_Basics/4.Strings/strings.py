c28adef027d28d464bfb519f3a7beb2e413019eb
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-04-08 17:10:44 +0300

    Initial commit

---
+++ 01.Python_Basics/4.Strings/strings.py
@@ -0,0 +1,17 @@
+# This code demonstrates how to work with strings in Python. It shows how to define strings using both single and double quotes, as well as how to use triple quotes for multi-line strings. Additionally, it illustrates string slicing to access specific parts of a string.
+
+course = "Python's course for Beginners"
+print('1. ' + course)
+course_2 = 'Python for "Beginners"'
+print('2. ' + course_2)
+email = '''
+Hi John,
+This is my first email to you.
+
+Regards,
+The hiring team
+'''
+print('3. ' + email)
+
+name = 'Jennifer'
+print('4. ' + name[1:-1])
