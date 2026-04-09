331af5761f10a2b0de45cac9e1f2da2a4fa0f162
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-04-09 16:15:23 +0300

    Initial commit

---
+++ 01.Python_Basics/7.Arithmetic Operations/arithmetic_operations.py
@@ -0,0 +1,12 @@
+# In Python, we have the following arithmetic operators: +, -, *, /, //, %, **, representing addition, subtraction, multiplication, division, floor division, modulus, and exponentiation respectively. We also have augmented assignment operators such as +=, -=, *=, /=, //=, %=, **= which are used to perform an operation and assign the result back to the variable in a single step.
+
+# The following below code shows an example of augmented assignment operator for subtraction.
+
+x = 10
+x -= 3
+print(x)
+
+# The following example demonstrates operator precedence in Python. The order of operations is determined by the precedence of the operators, which can be remembered using the acronym PEMDAS (Parentheses, Exponents, Multiplication and Division, Addition and Subtraction).
+
+x = (2 + 3) * 10 - 3
+print(x)
