89ccee2c1dcee6e76f4ad06596640ebd709c92e4
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-04-09 16:23:47 +0300

    Initial commit

---
+++ 01.Python_Basics/8.Math Functions/math_functions.py
@@ -0,0 +1,8 @@
+# The following code demonstrates the use of some mathematical functions in Python. It imports the math module, defines a variable x with the value 2.9, and then prints the absolute value of -2.9, the ceiling of x, and the floor of x.
+
+import math
+
+x = 2.9
+print(abs(-2.9))
+print(math.ceil(x))
+print(math.floor(x))
