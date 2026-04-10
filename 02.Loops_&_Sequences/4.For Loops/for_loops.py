545f23e6d71d0b806eb8200d1798e338b5c3da89
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-04-10 17:37:59 +0300

    Initial commit

---
+++ 02.Loops_&_Sequences/4.For Loops/for_loops.py
@@ -0,0 +1,8 @@
+# The following code calculates the total price of items in a list using a for loop.
+
+prices = [10, 20, 30]
+total = 0
+
+for price in prices:
+    total += price
+print(f"Total: {total}")
