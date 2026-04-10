d400ffa0c62aacfdc598216ef6ef083016c6f979
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-04-10 15:37:24 +0300

    Initial commit

---
+++ 02.Loops_&_Sequences/1.While Loops/while_loops.py
@@ -0,0 +1,13 @@
+# The following code demonstrates the use of while loops in Python. The first loop prints the numbers from 1 to 5, and the second loop prints a pattern of asterisks.
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+print("Done")
+
+i = 1
+while i <= 5:
+    print("*" * i)
+    i += 1
+print("Done")
