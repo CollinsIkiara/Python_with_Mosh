0a929e9e5fbc9a8419c33b397701f819c1db570b
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-04-13 18:05:13 +0300

    Initial commit

---
+++ 04.Functions/6.Comments/comments.py
@@ -0,0 +1,14 @@
+# This is a comment in Python. Comments are ignored by the interpreter and are used to explain code.
+
+# prints Sky is blue
+print("Sky is blue")
+
+# prints message to the console
+print("Ocean is blue")
+
+# Important notes on comments:
+# 1. Comments can be single-line or multi-line.
+# 2. Single-line comments start with a hash symbol (#).
+# 3. Multi-line comments can be created using triple quotes (''' or """).
+# 4. Comments shouldn't explain the obvious, (e.g., like the examples above) but should provide insight into why and how the code is doing something, not what it is doing.
+# 5. Comments should be written in such a way that if the code changes, there's no need to change the comment. This means that comments should focus on the intent and purpose of the code rather than describing the code itself, i.e., in the first example above, if the print statement changes to print("Sky is green"), the comment would be outdated and misleading.
