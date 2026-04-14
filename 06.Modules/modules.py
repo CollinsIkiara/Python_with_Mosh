7b3d0d547a2b67c76e98fd5b6757eaff3048772c
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-04-14 12:24:58 +0300

    Initial commit

---
+++ 06.Modules/modules.py
@@ -0,0 +1,7 @@
+# This is the main module that uses the find_max function from the utils module. 
+
+from utils import find_max
+
+numbers = [10, 6, 3, 2]
+maximum = find_max(numbers)
+print(maximum)
